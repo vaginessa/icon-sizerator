1682a05aed156c8fc5ea4364469111f48f741689
David J Peacock <david@davidjpeacock.ca>
2015-01-10 23:37:58 -0500

    about ready to give up on meteor

--- icon-sizerator.js
+++ icon-sizerator.js
@@ -1,70 +1,11 @@
 var im = Meteor.npmRequire('imagemagick');
 
 if (Meteor.isClient) {
-  var sourceImage = process.argv[2];
 
-  var sizes = {
-    "config": {
-      "directory": "outputs/",
-      "prefix": "icon",
-      "suffix": ".png",
-      "suffixRetina":"@2x.png"
-    },
-    "data": [
-      { "size": 29, "customDefault": "-small" },
-      { "size": 40 },
-      { "size": 50 },
-      { "size": 57, "customDefault": "" },
-      { "size": 60 },
-      { "size": 72 },
-      { "size": 76 }
-    ]
-  };
-
-
-  sizes.data.map(function (value, index) {
-    // Output directory
-    var outDir = sizes.config.directory;
-
-    // Image name changes if `customDefault` is defined
-    var imageName = sizes.config.prefix;
-    imageName += (typeof value.customDefault) !== 'undefined' ? value.customDefault : '-' + value.size;
-
-    // Image suffix and extension for retina and non-retina
-    var suffix = sizes.config.suffix;
-    var suffixRetina = sizes.config.suffixRetina;
-
-    // ImageMagick options for non-retina
-    var options = {
-      srcPath: sourceImage,
-      quality: 1,
-      dstPath: outDir + imageName + suffix,
-      width: value.size
-    };
-
-    // ImageMagick options for retina
-    var optionsRetina = {
-      srcPath: sourceImage,
-      quality: 1,
-      dstPath: outDir + imageName + suffixRetina,
-      width: (value.size)*2
-    };
-
-
-    // Process non-retina icons
-    im.resize(options, function (err) {
-      if (err) { throw err; }
-    });
-
-    // Process retina icons
-    im.resize(optionsRetina, function (err) {
-      if (err) { throw err; }
-    });
-  });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  Meteor.startup(function() {
     // code to run on server at startup
   });
 }
