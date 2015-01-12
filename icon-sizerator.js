f38427a182fdfb38f9a96973f52ea9c240bc3319
David J Peacock <david@davidjpeacock.ca>
2015-01-11 21:19:52 -0500

    added logging capability

--- icon-sizerator.js
+++ icon-sizerator.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var im = require('imagemagick');
+var log = require('custom-logger').config({ level: 0 });
 
 http.createServer(function(req, res) {
   if (req.url == "/" || req.url == "/index.html") {
@@ -8,25 +9,25 @@ http.createServer(function(req, res) {
       'Content-Type': 'text/html'
     });
     res.end(fs.readFileSync('icon-sizerator.html'));
-    console.log("Sent html\n");
+    log.debug("Sent HTML");
   } else if (req.url == "/icon-sizerator.css") {
     res.writeHead(200, {
       'Content-Type': 'text/css'
     });
     res.end(fs.readFileSync('icon-sizerator.css'));
-    console.log("Sent css\n");
+    log.debug("Sent CSS");
   } else if (req.url == "/ei-image.svg") {
     res.writeHead(200, {
       'Content-Type': 'image/svg+xml'
     });
     res.end(fs.readFileSync('public/ei-image.svg'));
-    console.log("Sent svg\n");
+    log.debug("Sent SVG");
   } else if (req.url == "/ei-image.png") {
     res.writeHead(200, {
       'Content-Type': 'image/png'
     });
     res.end(fs.readFileSync('public/ei-image.png'));
-    console.log("Sent png\n");
+    log.debug("Sent PNG");
   } else {
     res.writeHead(404, {
       'Content-Type': 'text/plain'
@@ -34,4 +35,4 @@ http.createServer(function(req, res) {
     res.end("Page could not be found");
   }
 }).listen(3000, "127.0.0.1");
-console.log('Started HTTP Server on localhost port 3000.');
+log.info('Started HTTP Server on localhost port 3000.');
